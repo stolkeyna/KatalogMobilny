24ea0df5200323dbc3ebbe495206332bcccf4a1d
MariiaLysiana <MariiaLysiana@TE-GDA-D-D21E24>
2026-09-14 16:30:55 +0200

    Dodaj pliki projektów.

---
+++ KatalogMobilny/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace KatalogMobilny
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ KatalogMobilny/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace KatalogMobilny
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object? sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                Kliknieto.Text = $"Kliknięto {count} raz";
+            else
+                Kliknieto.Text = $"Kliknięto {count} razy";
+
+            SemanticScreenReader.Announce(Kliknieto.Text);
+        }
+    }
+}

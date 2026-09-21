e9e551f1407f98aee3ad79ef569270627a54b525
MariiaLysiana <MariiaLysiana@TE-GDA-D-D21E24>
2026-09-21 09:57:33 +0200

    Aaaaaa

--- KatalogMobilny/MainPage.xaml.cs
+++ KatalogMobilny/MainPage.xaml.cs
@@ -6,19 +6,26 @@
 
         public MainPage()
         {
+            //alternatywa tworzenia elementów interfejsu zamiast xamla
+            //tego nie robimy, tutaj tylko logika!
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object? sender, EventArgs e)
+        private void PokazButton(object sender, EventArgs e)
         {
-            count++;
-
-            if (count == 1)
-                Kliknieto.Text = $"Kliknięto {count} raz";
-            else
-                Kliknieto.Text = $"Kliknięto {count} razy";
-
-            SemanticScreenReader.Announce(Kliknieto.Text);
+            EtykietaWyniku.Text = "Wybrano processor";
         }
+
+        //private void OnCounterClicked(object? sender, EventArgs e)
+        //{
+        //    count++;
+
+        //    if (count == 1)
+        //        Kliknieto.Text = $"Kliknięto {count} raz";
+        //    else
+        //        Kliknieto.Text = $"Kliknięto {count} razy";
+
+        //    SemanticScreenReader.Announce(Kliknieto.Text);
+        //} 
     }
 }
